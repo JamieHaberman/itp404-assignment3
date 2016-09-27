5601fdada9e59fbfc5414378102d29cd7069652f
JamieHaberman <jamiehaberman@guest-wireless-nup-1620-10-121-065-089.usc.edu>
2016-09-27 11:02:38 -0700

    AJAX and Handlebars

---
+++ results.js
@@ -0,0 +1,33 @@
+function getSubreddits(subreddit) {
+  
+var url = 'https://www.reddit.com/r/'+subreddit+'.json';
+
+  var promise = $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json'
+    
+    
+    }).then(function(response){
+   console.log(response);
+        var templateSource = $('#subreddit-template').html();
+        var template = Handlebars.compile(templateSource);
+
+        var html = template({
+          data: response.data.children
+        });
+        
+        $('#response').html(html);
+         },function(){
+
+         console.log("there was an error");
+     
+   });
+  
+}
+
+       
+ 
+
+ 
+
